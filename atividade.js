1965c38c6b71d24f81348aaeab783e7f0d768382
yangray1515 <102757737+yangray1515@users.noreply.github.com>
2022-05-23 16:36:15 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,128 @@
+var aluno = document.querySelector('#primeiro-paciente');
+
+var nome = aluno.querySelector('.info-nome');
+console.log(nome)
+
+var valorPeso = aluno.querySelector('.info-peso');
+var peso = parseInt(valorPeso.textContent);
+console.log(peso)
+
+var valorAltura = aluno.querySelector('.info-altura');
+var altura = parseInt(valorAltura.textContent);
+console.log(altura)
+
+var valorgordura = aluno.querySelector('.info-gordura');
+var gordura = parseInt(valorgordura.textContent);
+console.log(gordura)
+
+var valorIMC = parseInt(peso+altura+gordura)/3
+console.log(valorIMC);
+
+
+ var subIMC = aluno.querySelector('.info-imc');
+ subIMC.textContent = valorIMC
+
+ //João
+
+ var aluno1 = document.querySelector('#segundo-aluno');
+
+var nome = aluno1.querySelector('.info-nome');
+console.log(nome)
+
+var valorPeso1 = aluno1.querySelector('.info-peso');
+var peso1 = parseInt(valorPeso1.textContent);
+console.log(peso1)
+
+var valorAltura1 = aluno1.querySelector('.info-altura');
+var altura1 =  parseInt(valorAltura1.textContent);
+console.log(altura1)
+
+var valorgordura1 = aluno1.querySelector('.info-gordura');
+var gordura1 = parseInt(valorgordura1.textContent);
+console.log(gordura1)
+
+var valorIMC1 = parseInt(peso1+altura1+gordura1) / 3
+console.log(valorIMC1);
+
+
+ var subIMC1 = aluno1.querySelector('.info-imc');
+ subIMC1.textContent = valorIMC1
+
+ //Erica
+
+ var aluno2 = document.querySelector('#terceiro-aluno');
+
+var nome = aluno2.querySelector('.info-nome');
+console.log(nome)
+
+var valorPeso2 = aluno2.querySelector('.info-peso');
+var peso2 = parseInt(valorPeso2.textContent);
+console.log(peso2)
+
+var valorAltura2 = aluno2.querySelector('.info-altura');
+var altura2 = parseInt(valorAltura2.textContent);
+console.log(altura2)
+
+var valorgordura2 = aluno2.querySelector('.info-gordura');
+var gordura2 = parseInt(valorgordura2.textContent);
+console.log(gordura2)
+
+var valorIMC2 = parseInt(peso2+altura2+gordura2) / 3
+console.log(valorIMC2);
+
+
+ var subIMC2 = aluno2.querySelector('.info-imc');
+ subIMC2.textContent = valorIMC2
+
+ //Douglas
+
+ var aluno3 = document.querySelector('#quarto-aluno');
+
+ var nome = aluno3.querySelector('.info-nome');
+ console.log(nome)
+ 
+ var valorPeso3 = aluno3.querySelector('.info-peso');
+ var peso3 = parseInt(valorPeso3.textContent);
+ console.log(peso3)
+ 
+ var valorAltura3 = aluno3.querySelector('.info-altura');
+ var altura3 = parseInt(valorAltura3.textContent);
+ console.log(altura3)
+ 
+ var valorgordura3 = aluno3.querySelector('.info-gordura');
+ var gordura3 = parseInt(valorgordura3.textContent);
+ console.log(gordura3)
+ 
+ var valorIMC3 = parseInt(peso3+altura3+gordura3)/3
+ console.log(valorIMC3);
+ 
+ 
+  var subIMC3 = aluno3.querySelector('.info-imc');
+  subIMC3.textContent = valorIMC3
+
+  //tatiana
+
+
+ var aluno4 = document.querySelector('#quinto-aluno');
+
+ var nome = aluno4.querySelector('.info-nome');
+ console.log(nome)
+ 
+ var valorPeso4 = aluno4.querySelector('.info-peso');
+ var peso4 = parseInt(valorPeso4.textContent);
+ console.log(peso4)
+ 
+ var valorAltura4 = aluno4.querySelector('.info-altura');
+ var altura4 = parseInt(valorAltura4.textContent);
+ console.log(altura4)
+ 
+ var valorgordura4 = aluno4.querySelector('.info-gordura');
+ var gordura4 = parseInt(valorgordura4.textContent);
+ console.log(gordura4)
+ 
+ var valorIMC4= parseInt(peso4+altura4+gordura4)/3
+ console.log(valorIMC4);
+ 
+ 
+  var subIMC4 = aluno4.querySelector('.info-imc');
+  subIMC4.textContent = valorIMC4
